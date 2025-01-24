ee2d8e17543a1561faa57c7ff24ec62229e9d006
Raul Faria <raul.faria@softwarehaus.net>
2025-01-24 17:25:46 +0100

    disable form login

--- security/section2/springsecsection2/src/main/java/ch/raulfaria/eazybank/EazyBankSecurityConfiguration.java
+++ security/section2/springsecsection2/src/main/java/ch/raulfaria/eazybank/EazyBankSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +15,7 @@ public class EazyBankSecurityConfiguration {
         return http.authorizeHttpRequests(requests -> requests
                         .requestMatchers("/account", "/balance", "/loans", "/cards").authenticated()
                         .requestMatchers("/notices", "/contact", "/error").permitAll())
-                .formLogin(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
